bba919ccd7cfbce5ab9c1d3687a922422871669e
fakeshadow <24548779@qq.com>
2025-12-20 16:50:59 +0800

    add readme

---
+++ README.md
@@ -0,0 +1,40 @@
+# async postgresql driver for [toasty ORM](https://github.com/tokio-rs/toasty)
+
+## Quick Start
+```rust
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    // define model with toasty
+    #[derive(Debug, toasty::Model)]
+    struct User {
+        #[key]
+        id: i32,
+        name: String,
+    }
+
+    // construct a xitca-postgres driver
+    let drv = xitca_postgres_toasty::PostgreSQL::new("postgres://postgres:postgres@localhost:5432")?;
+
+    // start the orm manager with our postgres driver
+    let orm = toasty::Db::builder()
+        .register::<User>()
+        .build(drv)
+        .await?;
+
+    // interact with database
+
+    orm.reset_db().await?;
+
+    User::create()
+        .id(123)
+        .name(String::from("john"))
+        .exec(&orm)
+        .await?;
+
+    let user = User::get_by_id(&orm, 123).await?;
+
+    assert_eq!(user.id, 123);
+    assert_eq!(user.name, "john");
+
+    Ok(())
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+#![doc = include_str!("../README.md")]
+
 mod async_iter;
 mod connection;
 mod driver;
@@ -7,42 +9,3 @@ mod value;
 type BoxedFuture<'a, T> = core::pin::Pin<Box<dyn core::future::Future<Output = T> + Send + 'a>>;
 
 pub use crate::driver::{Config, PostgreSQL};
-
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    #[tokio::test]
-    async fn connect() {
-        let drv = PostgreSQL::new("postgres://postgres:postgres@localhost:5432")
-            .unwrap()
-            .concurrency(123);
-
-        let db = toasty::Db::builder()
-            .register::<User>()
-            .build(drv)
-            .await
-            .unwrap();
-
-        db.reset_db().await.unwrap();
-
-        #[derive(Debug, toasty::Model)]
-        struct User {
-            #[key]
-            id: i32,
-            name: String,
-        }
-
-        User::create()
-            .id(123)
-            .name(String::from("john"))
-            .exec(&db)
-            .await
-            .unwrap();
-
-        let user = User::get_by_id(&db, 123).await.unwrap();
-
-        assert_eq!(user.id, 123);
-        assert_eq!(user.name, "john")
-    }
-}
